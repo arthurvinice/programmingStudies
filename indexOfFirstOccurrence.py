fbffe350d302c78c83dd176d353a89f7e34ee94f
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-04-23 11:07:41 -0300

    Update indexOfFirstOccurrence.py

--- indexOfFirstOccurrence.py
+++ indexOfFirstOccurrence.py
@@ -1,8 +1,11 @@
-# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Given two strings needle and haystack, return the index of the first
+# occurrence of needle in haystack, or -1 if needle is not part of haystack.
 # Challenge available in leetcode.com
 
 def strStr(haystack: str, needle: str) -> int:
     haystack.lower().find(needle)
+    if len(haystack) < len(needle):
+        return -1
     if needle in haystack:
         return haystack.index(needle)
     else:
