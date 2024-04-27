b25f2d3713b39485df7e93c7657202d10d4cb0b8
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-04-27 05:27:29 -0300

    Update usernameValidation.js

--- usernameValidation.js
+++ usernameValidation.js
@@ -1,4 +1,3 @@
-//A simple username validation for Codrebyte.com
 // Have the function CodelandUsernameValidation(str) take the
 // str parameter being passed and determine if the string is a valid 
 // username according to the following rules:
@@ -7,8 +6,9 @@
 // 2. It must start with a letter.
 // 3. It can only contain letters, numbers, and the underscore character.
 // 4. It cannot end with an underscore character.
-
 // If the username is valid then your program should return the string true, otherwise return the string false.
+
+// A challenge available on CoderByte.com
 
 function CodelandUsernameValidation(str) {
     const validate = true;
