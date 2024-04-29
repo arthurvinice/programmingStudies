7d1f4a933de775b5d850204dcd9473cd19097167
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-04-29 12:03:17 -0300

    Update description

--- singleNumber.py
+++ singleNumber.py
@@ -1,7 +1,5 @@
-# Given a non-empty array of integers nums,
-# every element appears twice except for one.
+# Given a non-empty array of integers nums, every element appears twice except for one.
 # Find that single one.
-
 # You must implement a solution with a linear runtime complexity
 # and use only constant extra space.
 # Challenge available in leetcode.com
