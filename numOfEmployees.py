99f8ef936a9732af842e281bb49c115b05bdbdcb
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-05-03 19:31:54 -0300

    comments

--- numOfEmployees.py
+++ numOfEmployees.py
@@ -1,3 +1,11 @@
+# There are n employees in a company, numbered from 0 to n - 1.
+# Each employee i has worked for hours[i] hours in the company.
+# The company requires each employee to work for at least target hours.
+# You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+# Return the integer denoting the number of employees who worked at least target hours.
+
+# Challenge available in leetcode.com
+
 def numberOfEmployeesWhoMetTarget(hours: list[int], target: int) -> int:
     if 1 <= len(hours) <= 50:
         count = 0
