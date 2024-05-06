4eb3d6733b15fa48689dd0ae2e4ea22d2727ec48
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-05-06 10:51:20 -0300

    Create sortedSquares.py

---
+++ sortedSquares.py
@@ -0,0 +1,8 @@
+def sortedSquares(nums: list[int]) -> list[int]:
+    if 1 <= len(nums) <= 10**4:
+        sqrs_array = []
+        for i in nums:
+            num = i**2
+            sqrs_array.append(num)
+        sqrs_array.sort()
+        return sqrs_array
