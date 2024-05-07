933a441ea925aac800a9a5b605f20c2e54cbd3f1
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-05-07 11:02:18 -0300

    comments

--- maxAchivableNumber.py
+++ maxAchivableNumber.py
@@ -1,3 +1,8 @@
+# An integer x is called achievable if it can become equal to num after applying the following operation no more than t times:
+# Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+# Return the maximum possible achievable number. 
+# It can be proven that there exists at least one achievable number.
+
 # Challenge available in leetcode.com
 
 def theMaximumAchievableX(num: int, t: int) -> int:
